edd8c951ad0f28c912e570e8b37562d4d76f7b3e
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2021-09-04 00:28:33 +0900

    java ArrayList

--- 3Ex/src/com/javalec/calex/ArrayListName.java
+++ 3Ex/src/com/javalec/calex/ArrayListName.java
@@ -3,29 +3,41 @@ package com.javalec.calex;
 import java.util.ArrayList;
 
 public class ArrayListName {
-	public static int getSurvivingIndex(int n, int k) {
-		//ArrayList 생성
-		ArrayList<Integer> soldiers = new ArrayList<>();
-		
-		//ArrayList 할당
-		for (int soldierNumber = 1; soldierNumber <= n; soldierNumber++) {
-			soldiers.add(soldierNumber);
-		}
-		
-		//죽는 순서 초기화
-		int killIndex = 0;
-		
-		//
-		while (soldiers.size() > 1) {
-			killIndex = (killIndex + k - 1) % soldiers.size();
+    public static int getSurvivingIndex(int n, int k) {
+        // 군사 배열리스트 생성
+        ArrayList<Integer> soldiers = new ArrayList<>();
+        // 시체 배열리스트 생성
+        ArrayList<Integer> corpse = new ArrayList<>();
+        for(int people = 1; people<=n; people++){
+            soldiers.add(people);
+        }
+        
+        //5번째 사람 죽이기 반복
+        
+        while(soldiers.size()>1){
+            //자결시키기
+            for(int soldiersIndex=1;soldiersIndex<=k-1;soldiersIndex++){
+                soldiers.add(soldiers.get(0));
+                soldiers.remove(0);
+                
+            }
+            
+            //시체 추가하기
+            corpse.add(soldiers.get(0));
+            soldiers.remove(0);
+            n--;
+        }
+        
+        //시체 나열
+        for(Integer corpseIndex : corpse){
+            System.out.println(corpseIndex + "번 군사가 죽습니다. ");
+        }
+        
+        //김신장군의 살 수 있는 자리 리턴
+        return soldiers.get(0);
+    }
 
-			System.out.println(soldiers.remove(killIndex) + "번 군사가 죽습니다.");
-		}
-		//리턴값으로 솔저의 0번 리턴
-		return soldiers.get(0);
-	}
-
-	public static void main(String[] args) {
-		System.out.println("김신은 " + getSurvivingIndex(20, 5) + "번 자리에 서있으면 됩니다.");
-	}
+    public static void main(String[] args) {
+        System.out.println("김신은 " + getSurvivingIndex(20, 5) + "번 자리에 서있으면 됩니다.");
+    }
 }
