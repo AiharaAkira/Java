de7e5470f59b978d247b190670635ff5994f7ff3
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2022-04-11 12:19:15 +0900

    JavaIntro

---
+++ Day0411/src/com/study/Test001.java
@@ -0,0 +1,9 @@
+package com.study;
+
+public class Test001 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ Test0411/src/com/test/TestExam.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		
+		// 콘솔 출력
+		System.out.println("여러분 사랑합니다.");
+		/* System.out.println("안녕하세요."); */
+		
+		/* 
+		 * \:t다음탭으로이동
+		 * \\ :\출력
+		 * \` :`출력
+		 * \" :"출력
+		 */
+		
+//		System.out.println(args[0]);
+	}
+
+}
