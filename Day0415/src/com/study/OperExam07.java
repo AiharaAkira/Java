c2825040da1fa26b3e89419b9a4a03f77d04962b
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2022-04-15 12:08:36 +0900

    Operator

---
+++ Day0415/src/com/study/OperExam07.java
@@ -0,0 +1,50 @@
+package com.study;
+
+import java.util.Scanner;
+
+public class OperExam07 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * int a = 10; int result = 0;
+		 * 
+		 * a=10; result = 0; result += a; System.out.println("result="+result);
+		 * 
+		 * 
+		 * a=10; result = 0; result -= a; System.out.println("result="+result);
+		 * 
+		 * 
+		 * a=10; result = 0; result *= a; System.out.println("result="+result);
+		 * 
+		 * 
+		 * a=10; result = 0; result /= a; System.out.println("result="+result);
+		 * 
+		 * 
+		 * a=10; result = 0; result %= a; System.out.println("result="+result);
+		 */
+		
+		
+		//세과목(국,영,수) 위 3과목의 점수를 입력받아 총점, 평균(소수2자리)을 출력
+		double koreanP ;
+		double englishP ;
+		double mathP  ;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("국어점수는?:");
+		koreanP = sc.nextDouble();
+		System.out.println("영어점수는?:");
+		englishP = sc.nextDouble();
+		System.out.println("수학점수는?:");
+		mathP = sc.nextDouble();
+		Double avg = ((koreanP+englishP+mathP)/3);
+		Double sum = (koreanP+englishP+mathP);
+		System.out.println("총점:"+sum+"점");
+		System.out.printf("평균:%.2f점\n",avg);
+		
+		
+		
+		
+	}
+
+}
